a5a7b65a35b2436e96899431c4389572551b7ff0
ashishmg <ashishmangla1991@gmail.com>
2020-11-26 15:59:08 +0530

    add navigation spa

--- 03spnavigation/main.js
+++ 03spnavigation/main.js
@@ -1,5 +1,22 @@
-const sections =  document.querySelectorAll('section');
-
-window.addEventListener('DOMContentLoaded', (event) => {
-    
+const sections = document.querySelectorAll("section");
+const options = {
+  threshold: 0.25,
+};
+window.addEventListener("DOMContentLoaded", (event) => {
+  const handleObserver = (entries, observer) => {
+    entries.forEach((entry) => {
+      console.log(entry);
+      if (entry.isIntersecting && entry.intersectionRatio >= 0.25) {
+        document.querySelector(".active").classList.remove("active");
+        const id = entry.target.getAttribute("id");
+        //console.log(id);
+        //console.log(document.querySelector(`[href]=#${id}`));
+        document.querySelector(`[href="#${id}"]`).classList.add("active");
+      }
+    });
+  };
+  const observerScroll = new IntersectionObserver(handleObserver, options);
+  sections.forEach((section) => {
+    observerScroll.observe(section);
+  });
 });
